efab09e7db22ebd6bfa2d4d37a1947a008a686c6
Trevor Nowalk <trevor.nowalk@gmail.com>
2017-11-21 04:27:32 +0000

    more attempts

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
+const os = require('os');
 const http = require('http');
 
-const hostname = 'bramble-n1.local';
+const hostname = os.hostname();
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.status(200).json({status: true});
+  //res.status(200).json({status: true});
+  res.writeHead(501, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ status: true }));
 });
 
 server.listen(port, hostname, () => {

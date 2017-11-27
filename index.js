75f38f4c06f7eaeaef9903b641e018f51e265401
Trevor Nowalk <trevor.nowalk@gmail.com>
2017-11-27 04:51:54 +0000

    Adding sonar support

--- index.js
+++ index.js
@@ -2,9 +2,26 @@
 
 const os = require('os');
 const http = require('http');
+const sonar = require('raspi-sonar/lib/sonar');
 
 const hostname = os.hostname();
-const port = 3000;
+const port = 3001;
+
+if (process.env.RABBIT_URL === undefined) {
+  console.info('No rabbit URL found, will not report sonar details');
+} else {
+  sonar.connect(process.env.RABBIT_URL).then((conn) => {
+    let opts = {
+      'name': 'raspi-mon',
+      'host': os.hostname()
+    };
+    let _ping = () => {
+      sonar.ping(conn, 'raspi-sonar.ping', JSON.stringify(opts));
+    };
+    _ping();
+    setInterval(_ping, 30000);
+  }).then(null, console.warn);
+}
 
 const server = http.createServer((req, res) => {
   //res.status(200).json({status: true});

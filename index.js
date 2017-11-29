4b5abd9cd05c44191c1b6af16029bd3601f2d8e1
Trevor Nowalk <trevor.nowalk@gmail.com>
2017-11-29 02:35:47 +0000

    Implementing latest sonar lib

--- index.js
+++ index.js
@@ -7,26 +7,14 @@ const sonar = require('raspi-sonar/lib/sonar');
 const hostname = os.hostname();
 const port = 3001;
 
-if (process.env.RABBIT_URL === undefined) {
-  console.info('No rabbit URL found, will not report sonar details');
-} else {
-  sonar.connect(process.env.RABBIT_URL).then((conn) => {
-    let opts = {
-      'name': 'raspi-mon',
-      'host': os.hostname()
-    };
-    let _ping = () => {
-      sonar.ping(conn, 'raspi-sonar.ping', JSON.stringify(opts));
-    };
-    _ping();
-    setInterval(_ping, 30000);
-  }).then(null, console.warn);
-}
+sonar.connect().then((conn) => {
+  sonar.pinger({ service: 'raspi-mon' });
+}).then(null, console.warn);
 
 const server = http.createServer((req, res) => {
   //res.status(200).json({status: true});
   res.writeHead(501, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ 
+  res.end(JSON.stringify({
     status: true,
     meta: {
         hostname: os.hostname()
